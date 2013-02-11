ebe93b4aba54e106ab4aa0b84d6f2b59b5fc0b8e
jgoza25 <jgoza25@yahoo.co.jp>
2013-02-11 22:29:30 +0900

    change image

--- src/jenkinsviewer/views/JenkinsView.java
+++ src/jenkinsviewer/views/JenkinsView.java
@@ -9,6 +9,7 @@ import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
@@ -85,56 +86,6 @@ public class JenkinsView extends ViewPart {
 		public void dispose() {
 		}
 		public Object[] getElements(Object parent) {
-			
-			Map<Node, Map<String, String>> result = new HashMap<Node, Map<String, String>>();
-			try {
-				IPreferenceStore store = Activator.getDefault().getPreferenceStore();
-				String p_url = store.getString(PreferenceConstants.P_URL);
-				URL url = new URL(p_url + "/api/xml?depth=2");
-				DocumentBuilder builder = DocumentBuilderFactory.newInstance()
-				        .newDocumentBuilder();
-				Document doc = builder.parse(url.openStream());
-				XPathFactory factory = XPathFactory.newInstance();
-				XPath xpath = factory.newXPath();
-				NodeList entries = (NodeList) xpath.evaluate(
-						"/hudson/job/*", doc, XPathConstants.NODESET );
-
-		        for( int i = 0; i < entries.getLength(); i++ ) {
-		        	Node n = entries.item(i);
-		        	Map<String, String> job = result.get(n.getParentNode());
-		        	if (job == null) { 
-		        		job = new HashMap<String, String>();
-		        	}
-		        	job.put(n.getNodeName(), n.getTextContent());
-		        	result.put(n.getParentNode(), job);
-		        }
-		        entries = (NodeList) xpath.evaluate(
-		                "/hudson/job/build[1]/timestamp", doc, XPathConstants.NODESET );
-		        for( int i = 0; i < entries.getLength(); i++ ) {
-		        	Node n = entries.item(i);
-		        	Map<String, String> job = result.get(n.getParentNode().getParentNode());
-		        	if (job == null) { 
-		        		job = new HashMap<String, String>();
-		        	}
-		        	Date execed = new Date(Long. parseLong(n.getTextContent()));
-		        	long sec = ((new Date()).getTime() - execed.getTime()) / 1000;
-		        	
-		        	if (sec < 60) {
-		        		job.put(n.getNodeName(), "たった今");
-		        	} else if (sec < 60 * 60) {
-		        		job.put(n.getNodeName(), String.format("%d分前", sec / 60));
-		        	} else if (sec < 60 * 60 * 24) {
-		        		job.put(n.getNodeName(), String.format("%d時間前", sec / 60 / 60));
-		        	} else if (sec < 60 * 60 * 24 * 30) {
-		        		job.put(n.getNodeName(), String.format("%d日前", sec / 60 / 60 / 24));
-		        	} else {
-		        		job.put(n.getNodeName(), String.format("%dヶ月前", sec / 60 / 60 / 24 / 30));
-		        	}
-		        	result.put(n.getParentNode().getParentNode(), job);
-		        }
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
 			return result.values().toArray();
 		}
 	}
@@ -145,14 +96,20 @@ public class JenkinsView extends ViewPart {
 		}
 		public Image getColumnImage(Object obj, int index) {
 			Map<String, String> job = (Map<String, String>)obj;
-			URL url = null;
-			try {
-				url = new URL("http://localhost:8080/jenkins/static/a0a462b7/images/16x16/" + job.get("color") +".png");
-			} catch (MalformedURLException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
+			String color = job.get("color");
+			ImageDescriptor id = null;
+			if (color.matches("blue")) {
+				id = Activator.getImageDescriptor("icons/blue.png");
 			}
-			ImageDescriptor id = ImageDescriptor.createFromURL(url);
+			if (color.matches("red")) {
+				id = Activator.getImageDescriptor("icons/red.png");
+			}
+			if (color.matches("yellow")) {
+				id = Activator.getImageDescriptor("icons/yellow.png");
+			}
+			if (id == null) {
+				id = Activator.getImageDescriptor("icons/grey.png");
+			}
 			return id.createImage();
 		}
 	}
@@ -163,23 +120,86 @@ public class JenkinsView extends ViewPart {
 	 * The constructor.
 	 */
 	public JenkinsView() {
+		doTimer();
 	}
+	
+	private static Map<Node,Map<String,String>> result = new ConcurrentHashMap<Node, Map<String, String>>();
 
 	// タイマーの実行メソッド
 	public void doTimer() {
 		Runnable runnable = new Runnable() {
 			public void run() {
-				Display.getDefault().timerExec(5000, new InnerTimerThread());
+				while (true) {
+					cacheElements();
+					Display.getDefault().asyncExec(new InnerTimerThread());
+					try {
+						Thread.sleep(10 * 1000);
+					} catch (InterruptedException e) {
+					}
+				}
 			};
 		};
-		runnable.run();
+		Thread a = new Thread(runnable);
+		a.start();
 	}
+	
+	public void cacheElements() {
+		try {
+			IPreferenceStore store = Activator.getDefault().getPreferenceStore();
+			String p_url = store.getString(PreferenceConstants.P_URL);
+			URL url = new URL(p_url + "/api/xml?depth=2");
+			DocumentBuilder builder = DocumentBuilderFactory.newInstance()
+			        .newDocumentBuilder();
+			Document doc = builder.parse(url.openStream());
+			XPathFactory factory = XPathFactory.newInstance();
+			XPath xpath = factory.newXPath();
+			NodeList entries = (NodeList) xpath.evaluate(
+					"/hudson/job/*", doc, XPathConstants.NODESET );
+
+	        for( int i = 0; i < entries.getLength(); i++ ) {
+	        	Node n = entries.item(i);
+	        	Map<String, String> job = result.get(n.getParentNode());
+	        	if (job == null) { 
+	        		job = new HashMap<String, String>();
+	        	}
+	        	job.put(n.getNodeName(), n.getTextContent());
+	        	result.put(n.getParentNode(), job);
+	        }
+	        entries = (NodeList) xpath.evaluate(
+	                "/hudson/job/build[1]/timestamp", doc, XPathConstants.NODESET );
+	        for( int i = 0; i < entries.getLength(); i++ ) {
+	        	Node n = entries.item(i);
+	        	Map<String, String> job = result.get(n.getParentNode().getParentNode());
+	        	if (job == null) { 
+	        		job = new HashMap<String, String>();
+	        	}
+	        	Date execed = new Date(Long. parseLong(n.getTextContent()));
+	        	long sec = ((new Date()).getTime() - execed.getTime()) / 1000;
+	        	
+	        	if (sec < 60) {
+	        		job.put(n.getNodeName(), "たった今");
+	        	} else if (sec < 60 * 60) {
+	        		job.put(n.getNodeName(), String.format("%d分前", sec / 60));
+	        	} else if (sec < 60 * 60 * 24) {
+	        		job.put(n.getNodeName(), String.format("%d時間前", sec / 60 / 60));
+	        	} else if (sec < 60 * 60 * 24 * 30) {
+	        		job.put(n.getNodeName(), String.format("%d日前", sec / 60 / 60 / 24));
+	        	} else {
+	        		job.put(n.getNodeName(), String.format("%dヶ月前", sec / 60 / 60 / 24 / 30));
+	        	}
+	        	result.put(n.getParentNode().getParentNode(), job);
+	        }
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return;
+	}
+
 
 	// タイマー内で使われる内部スレッドクラス
 	class InnerTimerThread implements Runnable {
 		public void run() {
 			viewer.setInput(getViewSite());
-			doTimer();
 		}
 	}
 	
